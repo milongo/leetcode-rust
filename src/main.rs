b60be07cd30bc74cb28f366c92148a0ee3411e2f
Emilio <16996940+milongo@users.noreply.github.com>
2024-06-13 16:24:53 -0400

    Function name change

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ use problems::three_sum::{self};
 fn main() {
     let nums = vec![1, 7, 9, 3, 4];
     let target = 30;
-    let result = three_sum::three_sum(nums, target);
+    let result = three_sum::three_sum_bool(nums, target);
     println!("Result for Problem: {:?}", result);
 }
