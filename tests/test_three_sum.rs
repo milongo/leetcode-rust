4260c91c52264f4b1d665f801dba74780c7101fb
Emilio <16996940+milongo@users.noreply.github.com>
2024-07-25 17:09:55 +0000

    Format

--- tests/test_three_sum.rs
+++ tests/test_three_sum.rs
@@ -2,7 +2,7 @@ use rust::problems::three_sum::{self, three_sum_bool};
 
 #[test]
 fn test_three_sum_example_1() {
-    let nums = vec![3,1 , 4, 7, 9];
+    let nums = vec![3, 1, 4, 7, 9];
     let target = 8;
     let expected = true;
     let result = three_sum::three_sum_bool(nums, target);
